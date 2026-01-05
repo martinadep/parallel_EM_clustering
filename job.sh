47acec73019010faa4a77aa6f5ca830b4bd67e22
jago <jago.revrenna@studenti.unitn.it>
2026-01-05 18:50:15 +0100

    fix

--- job.sh
+++ job.sh
@@ -12,4 +12,4 @@ cd $PBS_O_WORKDIR
 
 cd build
 
-mpirun -np 4 ./em_clustering -d ../datasets/gmm_100000p_3k.csv -k 3 -o ../results/out.csv
+mpirun.actual -np 4 ./em_clustering -d ../datasets/gmm_100000p_3k.csv -k 3 -o ../results/out.csv
