8075f19cb188288b4bbc97b3c2bbf54468bcd7a6
martinadep <martina.depiccoli@studenti.unitn.it>
2026-01-11 22:24:17 +0100

    fix

--- src/em_algorithm.c
+++ src/em_algorithm.c
@@ -71,30 +71,27 @@ void em_algorithm(T** data_points, int dim, int num_data_points,
 {
     T** resp = alloc_matrix(num_data_points, num_clusters);
     T prev_log_likelihood = -INFINITY;
-    T log_lik = 0.0;
-    
+    T log_lik; // dichiarata fuori dalla regione parallel
+
     #pragma omp parallel
     {
         for(int iter = 0; iter < MAX_ITER; iter++){
 
             /* ====================== E STEP ====================== */
-
             #pragma omp for schedule(static)
             for(int i = 0; i < num_data_points; i++){
                 T norm = 0.0;
-
                 for(int k = 0; k < num_clusters; k++){
                     T pdf = multiv_gaussian_pdf(data_points[i], dim,
                                                gmm[k].mean, gmm[k].cov);
                     resp[i][k] = gmm[k].weight * pdf;
                     norm += resp[i][k];
                 }
-
                 for(int k = 0; k < num_clusters; k++)
                     resp[i][k] /= norm;
             }
 
-            /* class_resp */
+            /* =================== CLASS_RESP ==================== */
             #pragma omp for schedule(static)
             for(int k = 0; k < num_clusters; k++){
                 T sum = 0.0;
@@ -102,7 +99,6 @@ void em_algorithm(T** data_points, int dim, int num_data_points,
                     sum += resp[i][k];
                 gmm[k].class_resp = sum;
             }
-
 
             /* ====================== M STEP ====================== */
 
@@ -112,37 +108,36 @@ void em_algorithm(T** data_points, int dim, int num_data_points,
                 gmm[k].weight = gmm[k].class_resp / num_data_points;
 
             /* mean */
-            #pragma omp for collapse(2) schedule(static)
-            for(int k = 0; k < num_clusters; k++)
-            for(int d = 0; d < dim; d++){
-                T sum = 0.0;
-                for(int i = 0; i < num_data_points; i++)
-                    sum += resp[i][k] * data_points[i][d];
-                gmm[k].mean[d] = sum / gmm[k].class_resp;
+            #pragma omp for schedule(static)
+            for(int k = 0; k < num_clusters; k++){
+                for(int d = 0; d < dim; d++){
+                    T sum = 0.0;
+                    for(int i = 0; i < num_data_points; i++)
+                        sum += resp[i][k] * data_points[i][d];
+                    gmm[k].mean[d] = sum / gmm[k].class_resp;
+                }
             }
 
             /* cov */
-            #pragma omp for collapse(3) schedule(static)
-            for(int k = 0; k < num_clusters; k++)
-            for(int i = 0; i < dim; i++)
-            for(int j = 0; j < dim; j++){
-                T sum = 0.0;
-                for(int n = 0; n < num_data_points; n++){
-                    T di = data_points[n][i] - gmm[k].mean[i];
-                    T dj = data_points[n][j] - gmm[k].mean[j];
-                    sum += resp[n][k] * di * dj;
+            #pragma omp for schedule(static)
+            for(int k = 0; k < num_clusters; k++){
+                for(int i = 0; i < dim; i++){
+                    for(int j = 0; j < dim; j++){
+                        T sum = 0.0;
+                        for(int n = 0; n < num_data_points; n++){
+                            T di = data_points[n][i] - gmm[k].mean[i];
+                            T dj = data_points[n][j] - gmm[k].mean[j];
+                            sum += resp[n][k] * di * dj;
+                        }
+                        gmm[k].cov[i][j] = sum / gmm[k].class_resp;
+                    }
+                    gmm[k].cov[i][i] += 1e-6; // regolarizzazione diagonale
                 }
-                gmm[k].cov[i][j] = sum / gmm[k].class_resp;
             }
 
-            /* regolarizzazione */
-            #pragma omp for collapse(2)
-            for(int k = 0; k < num_clusters; k++)
-            for(int i = 0; i < dim; i++)
-                gmm[k].cov[i][i] += 1e-6;
-
-
             /* ================= LOG-LIKELIHOOD ================= */
+            log_lik = 0.0;
+
             #pragma omp for reduction(+:log_lik) schedule(static)
             for(int n = 0; n < num_data_points; n++){
                 T prob = 0.0;
@@ -163,16 +158,15 @@ void em_algorithm(T** data_points, int dim, int num_data_points,
             }
 
             #pragma omp barrier
-        }
-    }
+        } // fine iterazioni
+    } // fine parallel
 
     /* ===================== LABELING ===================== */
-
     #pragma omp parallel for schedule(static)
-    for (int n = 0; n < num_data_points; n++) {
+    for(int n = 0; n < num_data_points; n++){
         int max_k = 0;
-        for (int k = 1; k < num_clusters; k++)
-            if (resp[n][k] > resp[n][max_k])
+        for(int k = 1; k < num_clusters; k++)
+            if(resp[n][k] > resp[n][max_k])
                 max_k = k;
         labels[n] = max_k;
     }
